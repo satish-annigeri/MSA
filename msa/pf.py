c0e731ff930a71e5395296a9a8a0cfab794c9bb6
Satish Annigeri <satish.annigeri@gmail.com>
2023-06-07 23:35:01 +0530

    Updated numpy style docstrings of functions in pf.py

--- msa/pf.py
+++ msa/pf.py
@@ -38,8 +38,10 @@ def pf_calclm(n, df_bc: pd.DataFrame):
 
     Returns
     -------
-    tuple(NDArrayInt, int)
-
+    lm : NDArrayInt
+         Integer array with degree of freedom number of each nodal displacement
+    nd : int
+         Number of degrees of freedom of the structure
     """
     lm = np.zeros((n, 3), dtype=np.int_)
     nd = 0
@@ -60,8 +62,23 @@ def pf_calclm(n, df_bc: pd.DataFrame):
 
 # Stiffness matrix in local coordinate system. Related to element
 def pf_stiff(E: float, A: float, Iz: float, L: float) -> NDArrayFloat:
-    """
-    Calculate stiffness of a plane frame member in local coordinate system
+    """Calculate stiffness of a plane frame member in local coordinate system
+
+    Parameters
+    ----------
+    E : float
+        Modulus of elasticity of material of the member
+    A : float
+        Cross-section area of the member
+    Iz : float
+        Second moment of area of the member about the axis of bending
+    L : float
+        Length of the member
+
+    Returns
+    -------
+    NDArrayFloat
+        Stiffness matrix of member about local axes
     """
     k = np.zeros((6, 6), dtype=float)
     k[0, 0] = float(E) * A / L
@@ -92,8 +109,17 @@ def pf_stiff(E: float, A: float, Iz: float, L: float) -> NDArrayFloat:
 
 # Rotation matrix. Related to element
 def pf_calcrot(dc):
-    """
-    Calculate rotation matrix of a plane frame member given its direction cosines
+    """Calculate rotation matrix of a plane frame member given its direction cosines
+
+    Parameters
+    ----------
+    dc : NDArrayFloat shape (2,)
+        Direction cosines of the member with reference to x-y axes
+
+    Returns
+    -------
+    NDArrayFloat shape (6, 6)
+        Rotation matrix to transform local stiffness matrix to global stiffness matrix
     """
     r = np.zeros((6, 6), dtype=float)
     cx = dc[0]
@@ -109,8 +135,19 @@ def pf_calcrot(dc):
 
 # Length and direction cosines of a member. Related to element
 def pf_calclen(xy1, xy2):
-    """
-    Calculate length and direction cosines of a plane frame member
+    """Calculate length and direction cosines of a plane frame member
+
+    Parameters
+    ----------
+    xy1 : NDArrayFloat shape (2,)
+        (x, y) coordinates of node 1
+    xy2 : NDArrayFloat shape (2,)
+        (x, y) coordinates of node 2
+
+    Returns
+    -------
+    float
+        Length of the member
     """
     delta = xy2 - xy1
     L = np.sqrt(np.sum(delta**2))
@@ -120,14 +157,46 @@ def pf_calclen(xy1, xy2):
 
 # Utility function to get number of end joints. Related to element
 def pf_get_endjts(imem, df_conn: pd.DataFrame):
-    """Return numbers of first and second end of a plane frame member"""
+    """Return numbers of first and second end of a plane frame member
+
+    Parameters
+    ----------
+    imem : int
+        Number of member
+    df_conn : pd.DataFrame
+        Connectivity matrix
+
+    Returns
+    -------
+    jt1 : int
+        Node1
+    jt2 : int
+        Node 2
+    """
     jt1, jt2 = df_conn.iloc[imem - 1, 0:2]
     return jt1, jt2
 
 
 # Utility function to get coordinates of end joints. Related to element
 def pf_get_endcoord(imem, df_xy, df_conn):
-    """Return the coordinates of the first and second end of a plane frame member"""
+    """Return the coordinates of the first and second end of a plane frame member
+
+    Parameters
+    ----------
+    imem : int
+        Number of the member
+    df_xy : pd.DataFrame
+        Coordinates of nodes
+    df_conn : pd.DataFrame
+        Connectivity of members
+
+    Returns
+    -------
+    xy1 : Float
+        Coordinate of node1
+    xy2 : Float
+        Coordinate of node2
+    """
     jt1, jt2 = pf_get_endjts(imem, df_conn)
     xy1 = df_xy.iloc[int(jt1) - 1, :]  # type: ignore
     xy2 = df_xy.iloc[int(jt2) - 1, :]  # type: ignore
@@ -136,7 +205,32 @@ def pf_get_endcoord(imem, df_xy, df_conn):
 
 # Utility function to get member properties. Related to element
 def pf_get_memprop(imem, df_xy, df_conn, df_mprop):
-    """Return the properties of specified plane frame member"""
+    """Return the material properties of specified plane frame member
+
+    Parameters
+    ----------
+    imem : int
+        Number of the member
+    df_xy : pd.DataFrame
+        Coordinates of nodes
+    df_conn : pd.DataFrame
+        Connectivity of members
+    df_mprop : pd.DataFrame
+        Material properties table
+
+    Returns
+    -------
+    E : float
+        Modulus of elasticity of the member
+    A : float
+        Cross-section area of member
+    Iz : float
+        Second moment of area of cross-section of the member about the axis of bending
+    L : float
+        Length of the member
+    dc : NDArrayFloat
+        Direction cosines of the member
+    """
     m = df_conn.iloc[imem - 1, 2] - 1
     E = df_mprop.iloc[m, 0]
     A = df_mprop.iloc[m, 1]
@@ -148,7 +242,22 @@ def pf_get_memprop(imem, df_xy, df_conn, df_mprop):
 
 # Utility function to get dof numbers at end joints. Related to element
 def pf_get_dof(imem, df_conn, lm):
-    """Return degree of freedom numbers of the ends of a plane frame member"""
+    """Return degree of freedom numbers of the ends of a plane frame member
+
+    Parameters
+    ----------
+    imem : int
+        Number of the member
+    df_conn : pd.DataFrame
+        Connectivity of members
+    lm : NDArrayFloat
+        Location matrix, specifying degree of freedom number for each node
+
+    Returns
+    -------
+    NDArrayInt
+        Degree of freedom numbers for node1 and node2
+    """
     jt1, jt2 = pf_get_endjts(imem, df_conn)
     memdof = np.array([0, 0, 0, 0, 0, 0])
     memdof[0:3] = lm[jt1 - 1, :]
@@ -158,9 +267,24 @@ def pf_get_dof(imem, df_conn, lm):
 
 # Stiffness matrix in structure coordinate system. Related to element
 def pf_gstiff(imem, df_xy, df_conn, df_mprop):
-    """
-    Calculate the stiffness matrix of a plane frame member in strucutre
+    """Calculate the stiffness matrix of a plane frame member in strucutre
     coordinate system
+
+    Parameters
+    ----------
+    imem : int
+        Number of the member
+    df_xy : pd.DataFrame
+        Coordinates of nodes
+    df_conn : pd.DataFrame
+        Connectivity of members
+    df_mprop : pd.DataFrame
+        Material properties table
+
+    Returns
+    -------
+    NDArray
+        Global stiffness matrix of the member
     """
     E, A, Iz, L, dc = pf_get_memprop(imem, df_xy, df_conn, df_mprop)
     r = pf_calcrot(dc)
@@ -170,9 +294,28 @@ def pf_gstiff(imem, df_xy, df_conn, df_mprop):
 
 # Assemble structure stiffness matrix, contribution from one member. Related to structure
 def pf_assemssm(imem, df_xy, df_conn, df_mprop, lm, ssm):
-    """
-    Superpose stiffness matrix of plane frame member on the structure
+    """Superpose stiffness matrix of plane frame member on the structure
     stiffness matrix
+
+    Parameters
+    ----------
+    imem : int
+        Number of the member
+    df_xy : pd.DataFrame
+        Coordinates of nodes
+    df_conn : pd.DataFrame
+        Connectivity of members
+    df_mprop : pd.DataFrame
+        Material properties table
+    lm : NDArrayFloat
+        Location matrix, specifying degree of freedom number for each node
+    ssm : NDArrayFloat
+        Structure stiffness matrix, initially a zero matrix
+
+    Returns
+    -------
+    NDArrayFloat
+        Structure stiffness matrix, after superposition of global stiffness of all members
     """
     K = pf_gstiff(imem, df_xy, df_conn, df_mprop)
     memdof = pf_get_dof(imem, df_conn, lm)
@@ -188,9 +331,28 @@ def pf_assemssm(imem, df_xy, df_conn, df_mprop, lm, ssm):
 
 # Assemble structure stiffness matrix, contribution from all members. Related to structure
 def pf_ssm(df_xy, df_conn, df_bc, df_mprop):
-    """
-    Assemble structure stiffness matrix by superposing stiffness matrices of
+    """Assemble structure stiffness matrix by superposing stiffness matrices of
     individual members
+
+    Parameters
+    ----------
+    df_xy : pd.DataFrame
+        Coordinates of nodes
+    df_conn : pd.DataFrame
+        Connectivity of members
+    df_bc : pd.DataFrame
+        Zero boundary conditions table, for nodes with one or more constrained degrees of freedom
+    df_mprop : pd.DataFrame
+        Material properties table
+
+    Returns
+    -------
+    ssm : NDArrayFloat
+        Structure stiffness matrix of the structure
+    lm : NDArrayInt
+        Location matrix, specifying degree of freedom number for each node
+    ndof : int
+        Number of degrees of freedom of the structure
     """
     n = len(df_xy)
     lm, ndof = pf_calclm(n, df_bc)
@@ -203,8 +365,21 @@ def pf_ssm(df_xy, df_conn, df_bc, df_mprop):
 
 # Assemble structure loadvector due to all joint loads. Related to structure
 def pf_assem_loadvec_jl(lm, df_jtloads, P):
-    """
-    Superpose joint loads on structure load vector
+    """Superpose joint loads on structure load vector
+
+    Parameters
+    ----------
+    lm : NDArrayInt
+        Location matrix, specifying degree of freedom number for each node
+    df_jtloads : pd.DataFrame
+        Nodal loads table
+    P : NDArrayFloat
+        Load vector
+
+    Returns
+    -------
+    NDArrayFloat
+        Modified load vector after superposing all nodal loads
     """
     nloads = len(df_jtloads)
     for iload in range(nloads):
@@ -219,9 +394,28 @@ def pf_assem_loadvec_jl(lm, df_jtloads, P):
 
 # Assemble structure loadvector due to one member load. Related to structure
 def pf_assem_loadvec_ml(iload, df_xy, df_conn, lm, df_memloads, P):
-    """
-    Superpose equivalent joint loads due to loads applied on members onto
+    """Superpose equivalent joint loads due to loads applied on members onto
     structure load vector
+
+    Parameters
+    ----------
+    iload : int
+        Number of member load
+    df_xy : pd.DataFrame
+        Coordinates of nodes
+    df_conn : pd.DataFrame
+        Connectivity of members
+    lm : NDArrayInt
+        Location matrix, specifying degree of freedom number for each node
+    df_memloads : pd.DataFrame
+        Member load table
+    P : Load vector
+        _description_
+
+    Returns
+    -------
+    NDArrayFloat
+        Modified load vector after superposing member load number `iload`
     """
     imem = int(df_memloads.iloc[iload - 1, 0])
     xy1, xy2 = pf_get_endcoord(imem, df_xy, df_conn)
@@ -241,9 +435,28 @@ def pf_assem_loadvec_ml(iload, df_xy, df_conn, lm, df_memloads, P):
 
 # Assemble structure loadvector due to all loads. Related to structure
 def pf_loadvec(df_xy, df_conn, df_jtloads, df_memloads, ndof, lm):
-    """
-    Assemble structure load vector due to joint loads and loads applied
+    """Assemble structure load vector due to joint loads and loads applied
     directly on members
+
+    Parameters
+    ----------
+    df_xy : pd.DataFrame
+        Coordinates of nodes
+    df_conn : pd.DataFrame
+        Connectivity of members
+    df_jtloads : pd.DataFrame
+        Nodal loads table
+    df_memloads : pd.DataFrame
+        Member load table
+    ndof : int
+        Number of degrees of freedom of the structure
+    lm : NDArrayInt
+        Location matrix, specifying degree of freedom number for each node
+
+    Returns
+    -------
+    NDArrayFloat
+        Modified load vector after superposing all member loads
     """
     P = np.zeros((ndof, 1), dtype=float)
     P = pf_assem_loadvec_jl(lm, df_jtloads, P)
@@ -255,8 +468,29 @@ def pf_loadvec(df_xy, df_conn, df_jtloads, df_memloads, ndof, lm):
 
 # Element end forces in one element. Related to element
 def pf_mem_endforces(imem, df_xy, df_conn, df_mprop, df_memloads, lm, x):
-    """
-    Calculate member end forces from joint displacements, for one chosen member
+    """Calculate member end forces from joint displacements, for one chosen member
+
+    Parameters
+    ----------
+    imem : int
+        Number of the member
+    df_xy : pd.DataFrame
+        Coordinates of nodes
+    df_conn : pd.DataFrame
+        Connectivity of members
+    df_mprop : pd.DataFrame
+        Material properties table
+    df_memloads : pd.DataFrame
+        Member load table
+    lm : NDArrayInt
+        Location matrix, specifying degree of freedom number for each node
+    x : NDArrayFloat
+        Displacement vector
+
+    Returns
+    -------
+    NDArrayFloat
+        Vector of member end forces
     """
     xy1, xy2 = pf_get_endcoord(imem, df_xy, df_conn)
     L, dc = pf_calclen(xy1, xy2)
@@ -401,9 +635,15 @@ def print_title(title: str):
 
 
 def print_mat(header: str, k: npt.NDArray[Any]) -> None:
-    """
-    Print the array k, preceded by a header string. Each element of k is
+    """Print the array k, preceded by a header string. Each element of k is
     printed using the format string fmt
+
+    Parameters
+    ----------
+    header : str
+        Header to be printed before printing the matrix
+    k : npt.NDArray[Any]
+        Matrix to be printed
     """
     m, n = k.shape
     print(f"{header} Size: {m} x {n}")
